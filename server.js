d6af2567970a6e0621ebcc47a194511ab8d5174d
Your Name <peesaprani2310@gmail.com>
2021-09-17 16:56:38 +0530

    zoom

---
+++ server.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const app = express();
+const server = require("http").Server(app);
+
+const { v4: uuidv4 } = require("uuid");
+app.set('view engine', 'ejs');
+
+const io = require("socket.io")(server, {
+    cors: {
+        origin: '*'
+    }
+});
+
+const { ExpressPeerServer } = require("peer");
+const peerServer = ExpressPeerServer(server, {
+    debug: true
+});
+
+app.use("/peerjs", peerServer);
+app.use(express.static("public"))
+
+
+
+app.get("/", function(req, res) {
+    res.redirect(`/${uuidv4()}`);
+});
+
+app.get("/:room", (req, res) => {
+    res.render("room", { roomId: req.params.room})
+})
+
+io.on("connection", (socket) => {
+    socket.on("join-room", (roomId, userId, userName) => {
+        socket.join(roomId);
+        socket.to(roomId).broadcast.emit("user-connected", userId);
+        socket.on("message", (message) => {
+            io.to(roomId).emit("createMessage", message, userName);
+        });
+    });
+});
+
+const port = process.env.PORT || 3030;
+server.listen(port, function() {
+    console.log("Server started")
+});
